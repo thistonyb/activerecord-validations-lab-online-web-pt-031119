42bbd20214c373e7228870c1a0d4124aa38e3229
Tony Baker <this.tonybaker@gmail.com>
2019-07-30 18:26:51 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,5 @@ class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :content, length: {minimum: 250}
   validates :summary, length: {maximum: 250}
-  validates :category, inclusion: {%w(Fiction Non-Fiction)}
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
 end

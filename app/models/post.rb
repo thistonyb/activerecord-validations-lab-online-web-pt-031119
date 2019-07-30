86f027f96a67ee34204660715756e388678815e5
Tony Baker <this.tonybaker@gmail.com>
2019-07-30 18:33:12 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,12 @@ class Post < ActiveRecord::Base
   validates :content, length: {minimum: 250}
   validates :summary, length: {maximum: 250}
   validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+  validate :clickbate?
+
+
+  def clickbate?
+    if self.title && !self.title.match(/Won't Believe|Secret|Top [\d*]|Guess/)
+      errors.add(:clickbate, "is not good enough")
+    end
+  end
 end

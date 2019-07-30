a0a4b79313dea41341b96f991a7159b86db43bcb
Tony Baker <this.tonybaker@gmail.com>
2019-07-30 18:34:16 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+  validate :clickbate?
+
+
+  def clickbate?
+    if self.title && !self.title.match(/Won't Believe|Secret|Top [\d*]|Guess/)
+      errors.add(:clickbate, "is not catchy enough!")
+    end
+  end
 end
